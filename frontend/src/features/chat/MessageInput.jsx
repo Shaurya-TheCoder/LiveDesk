be3deb9bca288e0565592821f8b3fe80b8d05dbe
Shaurya-TheCoder <guptashaurya115@gmail.com>
2026-09-14 19:26:54 +0530

    initialized the project with basic chat page

--- frontend/src/features/chat/MessageInput.jsx
+++ frontend/src/features/chat/MessageInput.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 function MessageInput({onSend, onTyping}){
     const [message, setMessage] = useState("");
 
+    const typingTimeoutRef = useRef(null);
+    const isTypingRef = useRef(false);
+
     const handleSubmit = (event) => {
         event.preventDefault();
 
@@ -13,17 +16,46 @@ function MessageInput({onSend, onTyping}){
 
         onSend(trimmedMessage);
         setMessage("");
+
+        onTyping(false);
     }
+
+    const handleTyping = (value) => {
+    if (value.length > 0) {
+        if (!isTypingRef.current) {
+            onTyping(true);
+            isTypingRef.current = true;
+        }
+
+        clearTimeout(typingTimeoutRef.current);
+
+        typingTimeoutRef.current = setTimeout(() => {
+            onTyping(false);
+            isTypingRef.current = false;
+        }, 500);
+    } else {
+        clearTimeout(typingTimeoutRef.current);
+
+        if (isTypingRef.current) {
+            onTyping(false);
+            isTypingRef.current = false;
+        }
+    }
+
+    setMessage(value);
+    };
+
+    useEffect(() => {
+        return () => {
+            clearTimeout(typingTimeoutRef.current);
+        };
+    }, []);
 
     return(
         <form className="chat-input" onSubmit={handleSubmit}> 
             <input type="text" 
             value={message} 
-            onChange={(event) => {
-                const value = event.target.value;
-                setMessage(value);
-                onTyping(message.length > 0);
-            }} 
+            onChange={(event) => handleTyping(event.target.value)} 
             placeholder="Type a message..." autoComplete="off" />
             <button type="submit"> Send </button> 
         </form>
